202d781999fd575ed00c998559cfbdf7d9706d65
Justin Johnson <justin@justinjohnson.io>
2022-04-10 08:13:19 -0500

    More tests

--- test/sharness/t0109-gateway-web-_redirects.sh
+++ test/sharness/t0109-gateway-web-_redirects.sh
@@ -14,23 +14,29 @@ test_launch_ipfs_daemon
 # Directory tree crafted to test _redirects file support
 test_expect_success "Add the _redirects file test directory" '
   mkdir -p testredirect/ &&
-  echo "index.html" > testredirect/index.html &&
-  echo "one.html" > testredirect/one.html &&
-  echo "two.html" > testredirect/two.html &&
-  echo "^/redirect-one$ /one.html" > testredirect/_redirects &&
-  echo "^/301-redirect-one$ /one.html 301" >> testredirect/_redirects &&
-  echo "^/302-redirect-two$ /two.html 302" >> testredirect/_redirects &&
-  echo "^/200-index$ /index.html 200" >> testredirect/_redirects &&
-  echo "^/*$ /index.html 200" >> testredirect/_redirects &&
+  echo "my index" > testredirect/index.html &&
+  echo "my one" > testredirect/one.html &&
+  echo "my two" > testredirect/two.html &&
+  echo "my 404" > testredirect/404.html &&
+  mkdir testredirect/redirected-splat &&
+  echo "redirected splat one" > testredirect/redirected-splat/one.html &&
+  echo "/redirect-one /one.html" > testredirect/_redirects &&
+  echo "/301-redirect-one /one.html 301" >> testredirect/_redirects &&
+  echo "/302-redirect-two /two.html 302" >> testredirect/_redirects &&
+  echo "/200-index /index.html 200" >> testredirect/_redirects &&
+  echo "/posts/:year/:month/:day/:title /articles/:year/:month/:day/:title 301" >> testredirect/_redirects &&
+  echo "/splat/:splat /redirected-splat/:splat 301" >> testredirect/_redirects &&
+  echo "/en/* /404.html 404" >> testredirect/_redirects &&
+  echo "/* /index.html 200" >> testredirect/_redirects &&
   REDIRECTS_DIR_CID=$(ipfs add -Qr --cid-version 1 testredirect)
 '
 
 REDIRECTS_DIR_HOSTNAME="${REDIRECTS_DIR_CID}.ipfs.localhost:$GWAY_PORT"
 
-test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/redirect-one redirects with default of 302, per _redirects file" '
+test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/redirect-one redirects with default of 301, per _redirects file" '
   curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/redirect-one" > response &&
   test_should_contain "one.html" response &&
-  test_should_contain "302 Found" response
+  test_should_contain "301 Moved Permanently" response
 '
 
 test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/301-redirect-one redirects with 301, per _redirects file" '
@@ -47,16 +53,58 @@ test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/302-redirect-two redire
 
 test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/200-index returns 200, per _redirects file" '
   curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/200-index" > response &&
-  test_should_contain "index.html" response &&
+  test_should_contain "my index" response &&
   test_should_contain "200 OK" response
 '
 
-test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/has-no-redirects-entry returns 404, since not in _redirects file" '
-  curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/has-no-redirects-entry" > response &&
+test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/posts/:year/:month/:day/:title redirects with 301 and placeholders, per _redirects file" '
+  curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/posts/2022/01/01/hello-world" > response &&
+  test_should_contain "/articles/2022/01/01/hello-world" response &&
+  test_should_contain "301 Moved Permanently" response
+'
+
+test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/splat/one.html redirects with 301 and splat placeholder, per _redirects file" '
+  curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/splat/one.html" > response &&
+  test_should_contain "/redirected-splat/one.html" response &&
+  test_should_contain "301 Moved Permanently" response
+'
+
+test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/en/has-no-redirects-entry returns custom 404, per _redirects file" '
+  curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/en/has-no-redirects-entry" > response &&
   test_should_contain "404 Not Found" response
+'
+
+test_expect_success "request for $REDIRECTS_DIR_HOSTNAME/catch-all returns 200, per _redirects file" '
+  curl -sD - --resolve $REDIRECTS_DIR_HOSTNAME:127.0.0.1 "http://$REDIRECTS_DIR_HOSTNAME/catch-all" > response &&
+  test_should_contain "200 OK" response
 '
 
 test_expect_success "request for http://127.0.0.1:$GWAY_PORT/ipfs/$REDIRECTS_DIR_CID/301-redirect-one returns 404, no _redirects since no origin isolation" '
   curl -sD - "http://127.0.0.1:$GWAY_PORT/ipfs/$REDIRECTS_DIR_CID/301-redirect-one" > response &&
   test_should_contain "404 Not Found" response
 '
+
+# DNSLink test requires a daemon in online mode with precached /ipns/ mapping
+# test_kill_ipfs_daemon
+# DNSLINK_FQDN="dnslink-test.example.com"
+# export IPFS_NS_MAP="$DNSLINK_FQDN:/ipfs/$CIDv1"
+# test_launch_ipfs_daemon
+
+# test_localhost_gateway_response_should_contain \
+#   "request for {dnslink}.ipns.localhost returns expected payload" \
+#   "http://$DNSLINK_FQDN.ipns.localhost:$GWAY_PORT" \
+#   "$CID_VAL"
+
+# # DNSLink test requires a daemon in online mode with precached /ipns/ mapping
+# test_kill_ipfs_daemon
+# DNSLINK_FQDN="dnslink-subdomain-gw-test.example.org"
+# export IPFS_NS_MAP="$DNSLINK_FQDN:/ipfs/$CIDv1"
+# test_launch_ipfs_daemon
+
+# test_hostname_gateway_response_should_contain \
+#   "request for {dnslink}.ipns.example.com returns expected payload" \
+#   "$DNSLINK_FQDN.ipns.example.com" \
+#   "http://127.0.0.1:$GWAY_PORT" \
+#   "$CID_VAL"
+
+test_done
